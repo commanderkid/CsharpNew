f19efe72a30f88ee6ff6f70a1f308a91702f7ac3
Alexey <nafiguspofigus@gmail.com>
2019-02-20 17:17:02 +0300

    Update 20.02.19_temp_file.cs

--- 20.02.19_temp_file.cs
+++ 20.02.19_temp_file.cs
@@ -2,42 +2,62 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TodoApplication
 {
     public class LimitedSizeStack<T>
     {
+        StackElement LinqToLastElement;
+
         StackElement temporaryLink;
         private int ACount = 0;
         private StackElement link = null;
+        private int Limit;
         private class StackElement
         {
             public StackElement Reference;
             public T Element { get; set; }
-
-            public StackElement(StackElement refer, T elem )
+            public StackElement LinqToMyself;
+            public StackElement(StackElement refer, StackElement linqToFutyre, T elem)
             {
+                LinqToMyself = linqToFutyre;
                 Reference = refer;
                 Element = elem;
             }
         }
 
-
         public LimitedSizeStack(int limit)
         {
+            if (limit <= 0)
+                throw new Exception();
+            Limit = limit;
+
         }
 
         public void Push(T item)
         {
+
             if (link == null)
-                link = new StackElement(null, item);
+            {
+                link = new StackElement(null, null, item);
+                LinqToLastElement = link;
+                ++ACount;
+            }
+            else if(ACount < Limit)
+            {
+                temporaryLink = link;
+                link = new StackElement(temporaryLink, null, item);
+                temporaryLink.LinqToMyself = link;
+                ++ACount;
+            }
             else
             {
                 temporaryLink = link;
-                link = new StackElement(temporaryLink, item);
+                link = new StackElement(temporaryLink, null, item);
+                temporaryLink.LinqToMyself = link;
+                LinqToLastElement = LinqToLastElement.LinqToMyself;
+                LinqToLastElement.Reference = null;
             }
-            ACount++;
         }
 
         public T Pop()
@@ -48,6 +68,7 @@ namespace TodoApplication
             {
                 temporaryLink = link;
                 link = link.Reference;
+                --ACount;
                 return temporaryLink.Element;
             }
         }
